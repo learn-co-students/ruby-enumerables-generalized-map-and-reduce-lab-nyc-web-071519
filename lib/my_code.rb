9c11f141ef49654da60b0a1501f52b0f2a819d7a
Alena Nikulina <alenanikulina0@gmail.com>
2019-07-12 18:14:26 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def my_map(source_array)
+    ary = []
+
+    self.each do |elem|
+      ary << yield(elem)
+    end
+
+    ary
+  end
+
+
+source_array.my_map {|n| n * -1}
+source_array.my_map {|n| n}
+source_array.my_map {|n| n * 2}
+source_array.my_map {|n| n * n}
+
+def reduce(source_array)
+
+end
+
+source_array.reduce(0) {|memo, n| memo + n}
+source_array.reduce(0) {|memo, n| memo + n}
+source_array.reduce(0) {|memo, n| memo && n}
+source_array.reduce(0) {|memo, n| memo || n}
