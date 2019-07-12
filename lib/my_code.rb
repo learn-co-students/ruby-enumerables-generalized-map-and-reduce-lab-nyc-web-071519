a82380b20f4075b0e834bdbd1b9e5e00eb8af869
Andrew Santos <asantos@alumni.nd.edu>
2019-07-12 03:05:25 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
-# Your Code Here
+def map (array)
+  count = 0 
+  new_array = []
+  while count < array.length do
+    new_array.push(yield(array[count]))
+    count+=1 
+  end
+  return new_array
+end
+
+def reduce (array, startingpoint = 0)
+  count = 0 
+  output = startingpoint
+  while count < array.length do 
+    if array[count]
+      output = yield(output, array[count])
+    else
+      output = false
+    end
+  #  if array[count] = true
+  #  else
+  #    return false
+  #  end
+    count += 1 
+  end 
+  output
+end
